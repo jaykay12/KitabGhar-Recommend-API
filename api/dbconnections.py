126d7fa6d9f547287956a50c8f4275eb3475145b
jaykay12 <jalazkumar1208@gmail.com>
2018-10-17 00:45:54 +0530

    Added DB Security by using .env file

--- api/dbconnections.py
+++ api/dbconnections.py
@@ -1,7 +1,13 @@
 import MySQLdb
+import os
+
+LOCALHOST = os.environ.get('LOCALHOST')
+USERNAME = os.environ.get('USERNAME')
+PASSWORD = os.environ.get('PASSWORD')
+DATABASE = os.environ.get('DATABASE')
 
 class DBConnections:
     def getCursorObject(self):
-        dbOBJ = MySQLdb.connect(host="localhost", user="root", passwd="jalaz", db="kitabghar")
+        dbOBJ = MySQLdb.connect(host=LOCALHOST, user=USERNAME, passwd=PASSWORD, db=DATABASE)
         curOBJ = dbOBJ.cursor()
         return curOBJ
